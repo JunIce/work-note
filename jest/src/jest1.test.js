83c34ef76ddc1ce29836a74fb7aeed36cc8fe956
JunIce <east.qrj@gmail.com>
2022-02-18 09:35:25 +0800

    test demo

--- jest/src/jest1.test.js
+++ jest/src/jest1.test.js
@@ -1,3 +1,15 @@
-test("two plus two is four", () => {
-    expect(2 + 2).toBe(4);
+test("test toBe", () => {
+  expect(2 + 2).toBe(4);
 });
+
+test("test equal", () => {
+  expect(2 * 2).toEqual(4)
+})
+
+test("test Truthiness", () => {
+  const n = null;
+  expect(n).toBeNull()
+  expect(n).toBeDefined()
+  expect(n).not.toBeTruthy()
+})
+
